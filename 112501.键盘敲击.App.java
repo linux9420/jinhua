df18a3fcd924c5909a2a16f3f10b2c8299c0aa0d
金華 <51697517+linux9420@users.noreply.github.com>
2020-11-25 20:08:35 +0800

    Create 112501.键盘敲击.App.java
    键盘点击事件初学

---
+++ 112501.键盘敲击.App.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class App {
+    JPanel myPanel=new JPanel();
+    JTextArea textArea_A=new JTextArea();
+    JTextArea textArea_B=new JTextArea();
+    //构造方法
+    public App() {
+        //textArea_A.setText("点击上↑键输出文字");
+        textArea_A.setBounds(150,100,300,150);
+        textArea_A.setBackground(Color.orange);
+        myPanel.add(textArea_A);
+        textArea_B.setBounds(150,300,300,150);
+        textArea_B.setBackground(Color.yellow);
+        myPanel.add(textArea_B);
+
+        textArea_A.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                super.keyPressed(e);
+                if (e.getKeyCode() == KeyEvent.VK_UP) {
+                    textArea_B.append("点击进入上流社会！\n");
+                }
+                if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                    textArea_B.setText("");
+                }
+            }
+        });
+    }
+    //显示窗体方法
+    void go(){
+        JFrame frame = new JFrame("嘤嘤嘤");
+        myPanel.setLayout(null);
+        frame.setContentPane(myPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(700,200,600,720);
+        //frame.pack();
+        frame.setVisible(true);
+    }
+    public static void main(String[] args) {
+        new App().go();
+    }
+}
