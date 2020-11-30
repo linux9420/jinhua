cec04b44e5430cd52e5512257d39cf17c9db6995
金華 <51697517+linux9420@users.noreply.github.com>
2020-11-30 12:01:17 +0800

    Create 113001.继承父类.R.java

---
+++ 113001.继承父类.R.java
@@ -0,0 +1,20 @@
+package demo;
+
+public class R {
+	
+	static void q() {
+		//System.out.println(233);
+		String boy_filePath = "img/boy.gif";
+		String girl_filePath = "img/girl.gif";
+		System.out.println(boy_filePath);
+		System.out.println(girl_filePath);
+		int [][]array2d = {{1,2,3},{4,5,6}};
+		for(int[] is:array2d) {
+			for(int i:is) {
+				System.out.print(i+" ");
+			}
+		}
+	}
+
+
+}
